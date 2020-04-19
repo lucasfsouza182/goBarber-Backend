ee0ffd7b0bbd7d886771e6f632b39192a124ae74
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-04-19 17:24:50 -0300

    Created user post route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-
-routes.get('/', (request, response) => response.json({ message: 'Opa' }));
+routes.use('/users', usersRouter);
 
 export default routes;
