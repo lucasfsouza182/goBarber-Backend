a9785ee67be7ca82e2a2aff55e8694440ef4bb3c
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-04-19 16:56:19 -0300

    Created foreing key appointment to user

--- src/database/migrations/1587259546289-CreateAppointments.ts
+++ src/database/migrations/1587259546289-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587259546289
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
