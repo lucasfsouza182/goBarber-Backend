01d6b4a90fb8b709c0c5b244505a5248c2e6af63
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-05-15 22:54:31 -0300

    Add Jest and create test to CreateAppointment

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12312312312',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12312312312');
+  });
+
+  /*   it('should not be able to create on the same time', () =>{
+    expect()
+  }) */
+});
