0057b5a5d3b40f220bb685b645ce905009dafc29
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-05-16 20:52:59 -0300

    Created authenticate user test and provider to hash operations

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,33 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUsersService from './CreateUsersService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUsersService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'teste@teste.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+});
