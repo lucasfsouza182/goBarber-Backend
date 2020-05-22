57ecdb32bb58ab4ff56e4aa45c1b1e255febf040
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-05-21 21:39:43 -0300

    Created migrations to token and controllers to password routes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,6 +10,7 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UsersTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -19,4 +20,9 @@ container.registerSingleton<IAppointmentsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UsersTokensRepository',
+  UsersTokensRepository,
 );
