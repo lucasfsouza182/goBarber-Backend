0b51ee787087ba48100f6d6d156529ced5d1196e
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-05-28 22:26:28 -0300

    Using celebrate to validate data on routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticate';
 import AppointmentsController from '../controllers/AppointmentsController';
@@ -10,13 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-/* appointmentsRouter.get('/', async (request, response) => {
-  const appointments = await appointmentsRepository.find();
-
-  return response.json(appointments);
-}); */
-
 appointmentsRouter.get('/me', providerAppointmentsController.index);
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
 
 export default appointmentsRouter;
