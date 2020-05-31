24f0f7c30aae8a93e2577544afa66a87c14e0fe3
Lucas Ferreira <lucasfsouza12@gmail.com>
2020-05-31 19:57:37 -0300

    Created appointments cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,19 +27,22 @@ class ListProviderAppointmentsService {
     month,
     year,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asd');
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-    console.log(cacheData);
-    const appointments = await this.appointmentsRepository.findAllInDayProvider(
-      {
+    let appointments = await this.cacheProvider.recover<Appointment[]>(
+      cacheKey,
+    );
+
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayProvider({
         provider_id,
         day,
         month,
         year,
-      },
-    );
+      });
 
-    await this.cacheProvider.save('asd', 'asd');
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
